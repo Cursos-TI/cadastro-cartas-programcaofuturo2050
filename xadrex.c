03fa4c0128988ed26da070cb1791e773f87cd81c
programcaofuturo2050 <yanhenriquesantos97@gmail.com>
2025-09-23 18:07:33 +0000

    COMMIT_EDITMSG

--- xadrex.c
+++ xadrex.c
@@ -2,12 +2,12 @@
 
 int main(){
 
-  char estado_1[3];        
+  char estado_1[5];        
   char codigo_1[10];      
-  char cidade_1[50];        
+  char cidade_1[20];        
   int populacao_1, pontos_1;
   float area_1, pib_1;
-
+  
   printf("CRIE A SUA PRÓXIMA CARTA. \n");
 
   printf("Digite o Estado 01: \n");
@@ -25,21 +25,24 @@ int main(){
   printf("Digite o Ponto Turístico 01: \n");
   scanf("%d", &pontos_1);
  
+  float densidade_1 = (float) populacao_1 / area_1;
+  float pibp_1 = pib_1 / populacao_1;
+
   printf("Estado: %s\n", estado_1);
   printf("Código: %s\n", codigo_1);
   printf("Cidade: %s\n", cidade_1);  
   printf("População: %d\n", populacao_1);
-  printf("Área: %f km²\n", area_1);
+  printf("Área: %.2f km²\n", area_1);
   printf("PIB: R$ %f bilhões\n", pib_1);
   printf("Pontos Turísticos: %d\n", pontos_1);
+  printf("A Densidade Demografica é: %f hab/km²\n", densidade_1); 
+  printf("O PIB PER CAPITA É: %f\n", pibp_1);
 
- 
-  char estado_2[3];        
-  char codigo_2[10];      
-  char cidade_2[50];        
+  char estado_2[100];        
+  char codigo_2[100];      
+  char cidade_2[100];        
   int populacao_2, pontos_2;
   float area_2, pib_2;
-
   printf("CRIE A SUA PRÓXIMA CARTA. \n");
 
   printf("Digite o Estado 02: \n");
@@ -57,14 +60,17 @@ int main(){
   printf("Digite o Ponto Turístico 02: \n");
   scanf("%d", &pontos_2);
  
+  float densidade_2 = (float) populacao_2 / area_2;
+  float pibp_2 = pib_2 / populacao_2;
   printf("Estado: %s\n", estado_2);
   printf("Código: %s\n", codigo_2);
   printf("Cidade: %s\n", cidade_2);  
   printf("População: %d\n", populacao_2);
-  printf("Área: %f km²\n", area_2);
+  printf("Área: %.2f km²\n", area_2);
   printf("PIB: R$ %f bilhões\n", pib_2);
   printf("Pontos Turísticos: %d\n", pontos_2);
-  
+  printf("A Densidade Demografica é: %2.f hab/km²\n", densidade_2); 
+  printf("O PIB PER CAPITA É: %f\n", pibp_2);
 return 0;
   
 }
